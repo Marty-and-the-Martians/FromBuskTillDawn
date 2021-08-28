84936448644564c15ea7598a8299ca451c63e0d3
Keanu Hasty <keanu.hasty@gmail.com>
2021-08-28 12:28:21 -0600

    one example route

---
+++ server/services/user.service.js
@@ -0,0 +1,15 @@
+const { user } = require('../../database');
+
+const getOne = async () => {
+  // filter data off the req here
+  // if you need to use any helper functions this is where its done.
+  // formating data, dates, etc...
+  const singleUser = user.read();
+
+  // add anything to the response here
+  // return response here
+};
+
+module.exports = {
+  getOne,
+};

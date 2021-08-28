0ed3c2280f807a3bff2b007791d21ecb2693e392
jordan-trimarchi <ajordant47@gmail.com>
2021-08-28 12:14:31 -0600

    Created Mongoose Schema

---
+++ database/model/index.js
@@ -0,0 +1,76 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  performer: {
+    type: Boolean,
+    default: false,
+    required: true
+  },
+  zipcode: {
+    type: Number,
+    required: true
+  },
+  photo: String,
+  bio: String,
+  followers: Number,
+  cashappURL: String,
+  following: [{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  }],
+  hostedEvents: [{
+    event: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Event'
+    }
+  }],
+  attendingEvents: [{
+    event: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Event'
+    }
+  }]
+});
+
+const EventSchema = new Schema({
+  time: {
+    type: Date,
+    required: true
+  },
+  owner: {
+    type: Number,
+    required: true
+  },
+  location: {
+    lat: Number,
+    lng: Number
+  },
+  genre: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = {
+  User: mongoose.model("User", UserSchema),
+  Event: mongoose.model("Event", EventSchema)
+}

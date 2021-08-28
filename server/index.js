a912d6c085aa86e57ea746d59b13836f945b31b9
Keanu Hasty <keanu.hasty@gmail.com>
2021-08-27 22:35:03 -0600

    repo initialized

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+// const queries = require('../database/schema.js'); // Emma's guide
+const cors = require('cors');
+
+app.use(express.static(__dirname + '/../dist'));
+
+// middleware
+// use express.json for parsing JSON
+app.use(express.json());
+// parse form encoded data
+app.use(express.urlencoded({ extended: true }));
+// use cors middleware for enabling CORS with various options
+app.use(cors());
+
+// get entries  -- from Emma's guide -- mongodb connection required
+// app.get('/entries', (req, res) => {
+//  queries.getEntry(req.query.name, (err, entry) => {
+//    if (err) {
+//      res.status(404).send(err);
+//    } else {
+//      res.status(200).send(entry);
+//    }
+//  })
+// });
+
+// set port where server will listen
+const port = 3000;
+
+// tell server to listen on predefined port
+app.listen(port, () => {
+  console.log(`Express server listening on port: ${port}`);
+});
---
+++ webpack/base.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  // set mode to dev to set process.env.NODE_ENV on DefinePlugin to value development
+  mode: 'development',
+  // entry point for this application will be in client dir w/in index.js
+  entry: path.resolve('client', 'index.jsx'),
+  output: {
+    // output will be stored in dist dir
+    path: path.resolve('dist'),
+    // naming bundled file
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        // match & resolve js & jsx files w/ their corresponding loader
+        test: /\.(js|jsx)$/,
+        // ignore transpiling node_modules
+        exclude: /node_modules/,
+        // use the babel loader for transpiling code
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        // pure CSS (i.e. w/o CSS modules)
+        test: /\.css$/i,
+        exclude: /\.module\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      root: path.resolve(__dirname, '../'),
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+};

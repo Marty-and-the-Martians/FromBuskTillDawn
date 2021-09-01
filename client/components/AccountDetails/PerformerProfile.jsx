ba7fe5cf038ce4316364ff5a2be65c3de3d8c9da
JJ Marquis <76982850+JJMrqs@users.noreply.github.com>
2021-08-31 22:06:24 -0600

    creates user account and performer profile pages

---
+++ client/components/AccountDetails/PerformerProfile.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const PerformerProfile = (props) => {
+  const { profileInfo } = props;
+  console.log(profileInfo);
+  const {
+    name, performer, followers, hostedEvents, bio, cashappURL, zipcode, photo, email,
+  } = profileInfo;
+
+  const events = hostedEvents.map((event) => (
+    <div key={event._id}>{`Upcoming Performances: ${event.description} at ${event.time}`}</div>
+  ));
+
+  return (
+    <div>
+      <h3>Account Settings</h3>
+      <button type="button">edit profile</button>
+      <img src={photo} alt="profile avatar" />
+      <div>{`Name: ${name}`}</div>
+      <div>{`payment: ${cashappURL}`}</div>
+      <div>{`Email: ${email}`}</div>
+      <div>{`Bio: ${bio}`}</div>
+      {performer
+        ? (
+          <>
+            <div>{`Follower Count: ${followers}`}</div>
+            <div>{`Home Zipcode: ${zipcode}`}</div>
+            {events}
+          </>
+        )
+        : <div>{`Home Zipcode: ${zipcode}`}</div>}
+    </div>
+  );
+};
+export default PerformerProfile;

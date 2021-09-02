ba595de00ee089bec43eb14ff3ebb9fad0c16655
jordan-trimarchi <ajordant47@gmail.com>
2021-09-02 10:51:44 -0600

    reformats myAccount to use currentUser data and conditionally renders followers and cashapp URL if performer

--- client/components/AccountDetails/MyAccount.jsx
+++ client/components/AccountDetails/MyAccount.jsx
@@ -8,28 +8,23 @@ const MyAccount = (props) => {
     name, performer, followers, hostedEvents, bio, cashappURL, zipcode, photo, email,
   } = profileInfo;
 
-  const events = hostedEvents.map((event) => (
-    <div key={event._id}>{`Upcoming Performances: ${event.description} at ${event.time}`}</div>
-  ));
-
   return (
     <div>
       <h3>Account Settings</h3>
       <button type="button">edit profile</button>
       <img src={photo} alt="profile avatar" />
       <div>{`Name: ${name}`}</div>
-      <div>{`payment: ${cashappURL}`}</div>
       <div>{`Email: ${email}`}</div>
       <div>{`Bio: ${bio}`}</div>
+      <div>{`Home Zipcode: ${zipcode}`}</div>
       {performer
         ? (
-          <>
+          <div>
             <div>{`Follower Count: ${followers}`}</div>
-            <div>{`Home Zipcode: ${zipcode}`}</div>
-            {events}
-          </>
+            <div>{`payment: ${cashappURL ? cashappURL : 'Add a cashapp link for tips!'}`}</div>
+          </div>
         )
-        : <div>{`Home Zipcode: ${zipcode}`}</div>}
+        : null}
     </div>
   );
 };

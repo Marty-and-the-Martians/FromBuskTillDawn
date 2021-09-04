4a2e366f24e8a331282e923c45861f11887e31a3
Keanu Hasty <keanu.hasty@gmail.com>
2021-09-04 10:25:36 -0600

    trying to get environment working

--- webpack/prod.js
+++ webpack/prod.js
@@ -25,8 +25,8 @@ module.exports = merge(base, {
     ],
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
     }),
   ],
 });

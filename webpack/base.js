9e5aa1c09f6aad704388eb1690aa579fad12ef1a
Keanu Hasty <keanu.hasty@gmail.com>
2021-09-04 10:09:44 -0600

    added production environment

--- webpack/base.js
+++ webpack/base.js
@@ -1,6 +1,7 @@
-const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   // set mode to dev to set process.env.NODE_ENV on DefinePlugin to value development
@@ -53,6 +54,9 @@ module.exports = {
       template: './index.html',
       filename: 'index.html',
       inject: 'body',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
     }),
   ],
 };
